7e092de4f073cb4871b91b9c74cd580bc1cfede7
erinknowles0501 <colorandcontrast@gmail.com>
2020-02-11 20:51:08 -0400

    Display card data

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    const url = 'https://public-api.wordpress.com/wp/v2/sites/dangstuffandjunk.home.blog/posts?_fields=id,date,link,content,categories,tags,jetpack_featured_media_url';
+    const url = 'https://public-api.wordpress.com/wp/v2/sites/dangstuffandjunk.home.blog/posts?_fields=id,date,link,title,content,categories,tags,jetpack_featured_media_url';
     fetch(url)
     .then(res => res.json())
     .then(data => this.setState({ data: data }));
--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Card = (props) => {
+	let content = props.data.content.rendered.replace('<p>','').replace('</p>','');
 	return (
-		<div>FUCK EH</div>
+		<div>
+			<div>
+				<h3>{props.data.title.rendered}</h3>
+				<img src={props.data.jetpack_featured_media_url} />
+			</div>
+			<div>
+				{content}
+			</div>
+		</div>
 	);
 }
 
