2d4744493f83dc32595395b2c808515c78f3255a
erinknowles0501 <colorandcontrast@gmail.com>
2020-02-11 20:34:25 -0400

    Initial commit and setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Card from './Card';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const url = 'https://public-api.wordpress.com/wp/v2/sites/dangstuffandjunk.home.blog/posts?_fields=id,date,link,content,categories,tags,jetpack_featured_media_url';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => this.setState({ data: data }));
+  }
+
+
+  render() {
+    if (this.state.data) {
+      return (
+        <main>
+          <Card />
+        </main>
+      );
+    } else {
+      return (
+        <main>
+          <h2>Loading...</h2>
+        </main>
+      )
+    }
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = (props) => {
+	return (
+		<div>FUCK EH</div>
+	);
+}
+
+export default Card
