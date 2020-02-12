a83216cdbc8a924ef628e34d245ad6af0f7e0a8f
erinknowles0501 <colorandcontrast@gmail.com>
2020-02-11 21:11:47 -0400

    More styling

--- src/Card.js
+++ src/Card.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 const Card = (props) => {
 	let content = props.data.content.rendered.replace('<p>','').replace('</p>','');
+	let cssString = 'url("' + props.data.jetpack_featured_media_url + '")';
+
+	let styles = {
+		backgroundImage: cssString
+	}
+
 	return (
 		<section>
 			<div>
 				<h3>{props.data.title.rendered}</h3>
-				<img src={props.data.jetpack_featured_media_url} />
+				<div className="img" style={styles}></div>
 			</div>
 			<div>
 				{content}
